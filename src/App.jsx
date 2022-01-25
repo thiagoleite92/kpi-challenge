84885a8804c93420674ffeab2657478437898b5f
Thiago Leite <leite.tjs@gmail.com>
2022-01-25 14:34:17 -0300

    feat: create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App () {
   return (
-    <div>
-      oi
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
