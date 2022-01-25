738565a84f55556a796ea6a0ba0dd00a8c746ede
Thiago Leite <leite.tjs@gmail.com>
2022-01-25 20:47:22 -0300

    style: create header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './header.css'
 
 function Header () {
   return (
-    <header>
-      Header
+    <header className='header'>
+      <div className='header-logo'>
+        logo
+      </div>
+      <div className='header-user-info'>
+        <div>
+          Nome
+        </div>
+        <div>
+          Email
+        </div>
+      </div>
     </header>
   )
 }
