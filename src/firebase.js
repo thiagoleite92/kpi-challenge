c4aadb8ea619a512539e1935851b1c0825b0b99c
Thiago Leite <leite.tjs@gmail.com>
2022-01-27 15:53:27 -0300

    feat: create firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAJjss-4TQbJC-BZQkmwU_kVkNADqjR05g',
+  authDomain: 'kpi-challenge-d2e74.firebaseapp.com',
+  projectId: 'kpi-challenge-d2e74',
+  storageBucket: 'kpi-challenge-d2e74.appspot.com',
+  messagingSenderId: '637638899891',
+  appId: '1:637638899891:web:1b61f4e8d89b3c112fd8c6'
+}
+
+export default firebaseConfig
