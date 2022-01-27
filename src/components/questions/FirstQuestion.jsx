551df0d8689f222a29c8de99c240a373c231987a
Thiago Leite <leite.tjs@gmail.com>
2022-01-26 22:29:23 -0300

    feat: first question save answer

--- src/components/questions/FirstQuestion.jsx
+++ src/components/questions/FirstQuestion.jsx
@@ -2,30 +2,60 @@ import React, { useContext } from 'react'
 import MainContext from '../../context/MainContext'
 
 function FirstQuestion () {
-  const { handleOptions } = useContext(MainContext)
+  const { handleOptions, answers: { firstQuestion } } = useContext(MainContext)
   return (
     <>
       <h1>Primeira Pergunta</h1>
       <form className='first-question'>
         <h4>Quantas pessoas tem na equipe que você está?</h4>
         <label className='first-question-label'>
-          <input type='radio' name='firstQuestion' value='1-3' onChange={handleOptions} />
+          <input
+            type='radio'
+            name='firstQuestion'
+            value='1-3'
+            onChange={handleOptions}
+            checked={firstQuestion === '1-3'}
+          />
           Entre 1 e 3
         </label>
         <label className='first-question-label'>
-          <input type='radio' name='firstQuestion' value='4-6' onChange={handleOptions} />
+          <input
+            type='radio'
+            name='firstQuestion'
+            value='4-6'
+            onChange={handleOptions}
+            checked={firstQuestion === '4-6'}
+          />
           Entre 4 e 6
         </label>
         <label className='first-question-label'>
-          <input type='radio' name='firstQuestion' value='7-9' onChange={handleOptions} />
+          <input
+            type='radio'
+            name='firstQuestion'
+            value='7-9'
+            onChange={handleOptions}
+            checked={firstQuestion === '7-9'}
+          />
           Entre 7 e 9
         </label>
         <label className='first-question-label'>
-          <input type='radio' name='firstQuestion' value='10-14' onChange={handleOptions} />
+          <input
+            type='radio'
+            name='firstQuestion'
+            value='10-1'
+            onChange={handleOptions}
+            checked={firstQuestion === '10-14'}
+          />
           Entre 10 e 14
         </label>
         <label className='first-question-label'>
-          <input type='radio' name='firstQuestion' value='15' onChange={handleOptions} />
+          <input
+            type='radio'
+            name='firstQuestion'
+            value='15'
+            onChange={handleOptions}
+            checked={firstQuestion === '15'}
+          />
           Mais de 15
         </label>
       </form>
