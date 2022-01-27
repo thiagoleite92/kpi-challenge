cda0626034295bf4e2ae43b3e5b27a2641b1e390
Thiago Leite <leite.tjs@gmail.com>
2022-01-26 22:16:41 -0300

    style: buttons now are able and disable to navigate between questions

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -4,12 +4,31 @@ import './questions.css'
 
 function Questions () {
   const [questionPosition, setQuestPosition] = useState(0)
+  const [sendButton, setSendButton] = useState(true)
+
   return (
     <main className='questions-container'>
       <MapQuestions questionPosition={questionPosition} />
       <div className='btn-question-container'>
-        <button onClick={() => setQuestPosition(questionPosition - 1)}>Anterior</button>
-        <button onClick={() => setQuestPosition(questionPosition + 1)}>Proxima</button>
+        <button
+          className='btn-prev-question'
+          disabled={questionPosition === 0}
+          onClick={() => setQuestPosition(questionPosition - 1)}
+        >
+          Anterior
+        </button>
+        <button
+          className='btn-send-answers'
+        >
+          Salvar e enviar
+        </button>
+        <button
+          disabled={questionPosition === 1}
+          onClick={() => setQuestPosition(questionPosition + 1)}
+          className='btn-next-question'
+        >
+          Proxima
+        </button>
       </div>
     </main>
   )
